a4395416f5ea7acf33ee4e712385d68326185d62
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-01-14 01:58:21 +0700

    Fix Bug UI bagian app.vue

--- src/App.vue
+++ src/App.vue
@@ -74,7 +74,7 @@
                           <path
                             d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
                         </svg>
-    
+
                       </a>
                     </button>
                     <button class="" @click="showModal = true">
@@ -84,21 +84,23 @@
                           <path
                             d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
                         </svg>
-  
+
                       </a>
                     </button>
                   </div>
                 </td>
               </tr>
             </tbody>
-            
+
           </table>
           <form v-if="selectedUser" @submit.prevent="updateUser(selectedUser.id)">
             <input v-model="selectedUser.nama" placeholder="Name">
             <input v-model="selectedUser.email" placeholder="Email">
             <input v-model="selectedUser.umur" placeholder="Age">
-            <button class="bg-blue-500 text-white p-2 rounded-md" type="submit">Save</button>
+            <button
+              type="submit">Save</button>
           </form>
+
         </div>
       </div>
 
